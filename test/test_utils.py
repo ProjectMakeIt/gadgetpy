9dc070af294883b7430dd8fea7e3ccd0f694da30
Blixa Morgan <blixa@projectmakeit.com>
2019-03-27 16:57:51 -0500

    try to fix tmpdir errors on travis?

--- test/test_utils.py
+++ test/test_utils.py
@@ -8,18 +8,21 @@ def test_valid_name():
 def test_invalid_name():
     assert not gadgetpy.utils.verifyName('test')
 
-def test_valid_pointer(fs,tmpdir):
-    fs.create_dir(str(tmpdir))
+def test_valid_pointer(fs):
+    tmpdir = "/gadgets/"
+    fs.create_dir(tmpdir)
     fs.create_file('/sys/class/udc/test')
-    assert gadgetpy.utils.verifyPointer(str(tmpdir),'test')
+    assert gadgetpy.utils.verifyPointer(tmpdir,'test')
 
-def test_pointer_in_use(fs,tmpdir):
-    gadget = gadgetpy.Gadget('testGadget',str(tmpdir),write=False)
+def test_pointer_in_use(fs):
+    tmpdir = "/gadgets/"
+    gadget = gadgetpy.Gadget('testGadget',tmpdir,write=False)
     fs.create_file(os.path.join(gadget.path,'UDC'),contents='test')
     fs.create_file('/sys/class/udc/test')
     assert not gadgetpy.utils.verifyPointer(str(tmpdir),'test')
 
-def test_invalid_pointer(fs,tmpdir):
-    fs.create_dir(str(tmpdir))
+def test_invalid_pointer(fs):
+    tmpdir = "/gadgets/"
+    fs.create_dir(tmpdir)
     fs.create_file('/sys/class/udc/test2')
-    assert not gadgetpy.utils.verifyPointer(str(tmpdir),'test')
+    assert not gadgetpy.utils.verifyPointer(tmpdir,'test')
